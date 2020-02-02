07795c7566551c84a4fabe1600c7e900b8b776b3
Geta Ureche <getairina@yahoo.com>
2020-02-02 11:26:13 +0200

    utilities

---
+++ src/main/java/com/records/records/utilities/Utilities.java
@@ -0,0 +1,21 @@
+package com.records.records.utilities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Utilities {
+
+    List<String> records = new ArrayList<String >();
+
+   public int findLengthMax(List<String> records[]){
+       int lengthMax = records.length;
+
+       for (int i = 1; i < records.length; i++) {
+           if ( lengthMax < records.length ){
+                   lengthMax = records.length;
+           }
+
+
+       }
+   }
+}
