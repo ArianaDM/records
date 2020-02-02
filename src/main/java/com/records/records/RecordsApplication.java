2b2603a2415fcea76ddc2dd877cadda8600adb88
ArianaDM <dumitrescuariana@yahoo.com>
2020-02-02 10:35:19 +0200

    first commit

---
+++ src/main/java/com/records/records/RecordsApplication.java
@@ -0,0 +1,14 @@
+package com.records.records;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordsApplication.class, args);
+		//test
+	}
+
+}
